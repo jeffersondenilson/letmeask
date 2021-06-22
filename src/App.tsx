21946fda725740a59e91f5b5e286213b3030c12a
Jefferson Denilson <jefersond121212@gmail.com>
2021-06-22 16:28:25 -0300

    paginas Home e NewRoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Home from './pages/Home'
+import NewRoom from './pages/NewRoom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Home />
   );
 }
 
